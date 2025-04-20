10a598c88a1b42ab2597f9659abeb51e0485e432
naufalparamarafif <profesoraiueo@gmail.com>
2025-04-21 02:05:07 +0700

    Lar ugha

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
+use App\Models\Customer;
+use App\Models\Product;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            // buatkan card total penjualan seluruh produk hari ini
+            Card::make('Total Penjualan', 'Rp ' . number_format(Transaction::sum('price_total')))
+                ->description('Jumlah seluruh penjualan')
+                ->descriptionIcon('heroicon-o-currency-dollar')
+                ->color('success'),
+
+            Card::make('Pelanggan Baru', Customer::whereDate('created_at', now())->count())
+                ->description('Hari ini')
+                ->descriptionIcon('heroicon-o-user-plus')
+                ->color('info'),
+
+            Card::make('Total Produk Terjual Hari Ini', TransactionDetail::whereDate('created_at', now())->sum('product_total') . ' Item')
+                ->description('Semua produk yang terjual hari ini')
+                ->descriptionIcon('heroicon-o-shopping-cart')
+                ->color('warning'),
+        ];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Widgets\StatsOverview;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -43,8 +44,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
+                StatsOverview::class,
+                \App\Filament\Widgets\UserGrowthCharte::class,
+                \App\Filament\Widgets\UserGrowthChart::class,
             ])
             ->middleware([
                 EncryptCookies::class,
