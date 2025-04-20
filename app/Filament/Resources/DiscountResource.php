cb95016d00ec184b1cb8755fd1b1419b79f8ee34
naufalparamarafif <profesoraiueo@gmail.com>
2025-04-20 22:55:10 +0700

    ntapps lah

--- app/Filament/Resources/DiscountResource.php
+++ app/Filament/Resources/DiscountResource.php
@@ -23,7 +23,7 @@ class DiscountResource extends Resource
 {
     protected static ?string $model = Discount::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-banknotes';
 
     public static function form(Form $form): Form
     {
